2363cad038c0fbf4c17551718c28a8f660e23d25
Gronkier <matteo.fabbri@gmail.com>
2014-11-04 20:49:18 +0100

    controller test

--- app/controllers/statControllers.js
+++ app/controllers/statControllers.js
@@ -1,10 +1,24 @@
 var statControllers = angular.module('statControllers', []);
 
-statControllers.controller('StatListCtrl', ['$scope', '$http',
-  function($scope, $http) {
+statControllers.controller('StatListCtrl', ['$scope', '$http', '$location',
+  function($scope, $http, $location) {
+	//init controller scope
+	$scope.yFrom = new Date().getFullYear();
+	$scope.yTo = $scope.yFrom ;
+	$scope.orderProp = 'pointsTot';
+	
+	
+	//$scope.getCurrentStats = function() {
+    //    getApiStats($scope, $http);
+    //};
+	
+	var protocol = $location.protocol().concat('://');
+	var host = $location.host();
 	$http({	method: 'GET', 
-			url: 'http://localhost:3003/api/stats/2014/2014/pointsTot' /*'http://localhost:3000/stats',
-			params: {yFrom: 2014, yTo: 2014, type: 'pointsTot' }*/
+			//url: 'http://localhost:3003/api/stats/2014/2014/pointsTot' //'http://localhost:3000/stats',
+			
+			url: protocol.concat(host,':3003/api/stats/',$scope.yFrom, '/',$scope.yTo,'/',$scope.orderProp)
+			//params: {"yFrom": '2014', "yTo": '2014', "type": 'pointsTot' }
 			})
 		.success(function(data) {
 			$scope.stats = data.stats;
@@ -14,5 +28,7 @@ statControllers.controller('StatListCtrl', ['$scope', '$http',
 			console.log('Error: ' + data);
 		});
 	
-    $scope.orderProp = 'pointsTot';
+    
   }]);
+  
+  
