818a264638757eabbd5b4447ba909bf5e441fc9b
Gronkier <matteo.fabbri@gmail.com>
2014-11-24 14:49:31 +0100

    auto hide hamburger

--- app/views/navbar/navbarControllers.js
+++ app/views/navbar/navbarControllers.js
@@ -5,6 +5,15 @@ var navbarControllers = angular.module('navbarControllers', []);
 navbarControllers.controller('navbarController', ['$scope', '$location',
     function($scope, $location)
     {
+        //Hamburger menu toggle
+        $(".navbar-nav li a").click(function (event) {
+            // check if window is small enough so dropdown is created
+            var toggle = $(".navbar-toggle").is(":visible");
+            if (toggle) {
+                $(".navbar-collapse").collapse('hide');
+            }
+        });
+
         $scope.isActive = function (viewLocation) {
             return viewLocation === $location.path();
         };
