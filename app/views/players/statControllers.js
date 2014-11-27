c6e1f64724bdfd245f73a8ef6cb8bb088c5493b4
Gronkier <matteo.fabbri@gmail.com>
2014-11-28 00:13:57 +0100

    trophy and stars

--- app/views/players/statControllers.js
+++ app/views/players/statControllers.js
@@ -17,7 +17,25 @@ statControllers.controller('statController', ['$scope', 'statService',
             });
         };
 
-    // $scope.setImage = function(imageUrl) {
+        $scope.getNumber = function(num) {
+            if (num > 0)
+                return new Array(num);
+            return [];
+        };
+        $scope.getNumberGold = function(num) {
+            var gold = Math.floor(num / 10);
+            return new Array(gold);
+        };
+        $scope.getNumberSilver = function(num) {
+            var silver = Math.floor((num - Math.floor(num / 10)*10)/5);
+            return new Array(silver);
+        };
+        $scope.getNumberBronze = function(num) {
+            var bronze = (num - Math.floor(num / 10)*10 -Math.floor((num - Math.floor(num / 10)*10)/5)*5);
+            return new Array(bronze);
+        };
+
+            // $scope.setImage = function(imageUrl) {
     //   $scope.mainImageUrl = imageUrl;
     // };
 
